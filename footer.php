06652b88eca8c42e22a418924cbba04f5426b88f
nuryneddy <82923383+nuryneddy@users.noreply.github.com>
2023-03-17 23:22:19 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer class="container-fluid text-center"><!-- div for the main image to stretch s to the edge of the page-->
+
+<div class="container"><!-- div to hold the content in the middle of the page-->
+    <div class="row"><!-- a row that gives us access to the BS columns-->
+        <div class="col-md-6 ">
+           <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span> </a>
+
+        </div>
+        <div class="col-md-6 ">
+            <p>Terms and Conditions</p>
+</div><!-- row-->
+    </div><!-- container -->
+</div><!-- container-fluid-->
+
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
