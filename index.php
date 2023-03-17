e38795bd2ddf2592f98f2ed713e7db43df7b42bf
nuryneddy <82923383+nuryneddy@users.noreply.github.com>
2023-03-17 23:22:30 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap" rel="stylesheet"> 
+
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="responsive.css" />
+
+<title>Title</title>
+</head>
+<body>
+
+
+  <header class="container-fluid bg1"> 
+    <nav class="navbar navbar-expand-md navbar-dark">
+    
+    <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
+  
+    
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+   
+   
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">PROJECTS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">PUBLICATION</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">CV</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">CONTACT</a>
+        </li>
+      </ul>
+
+
+      
+    </div>
+  </nav>
+
+
+<div class="container">
+    <div class="tag">
+              <h1> Games Design For The Future</h1>
+            </div>
+            </div>
+  
+  </header>
+
+  <?php get_header();?>   <!--  Tells WordPress to include header.php -->
+      <section class="container-fluid aboutbg text-center">
+        <div class="container">
+            <h2> GAMES &amp; LEVEL DESIGNER </h2>
+            <p class="particle">  .... and particle wizard</p>
+            <p class="about-text"> My name is josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects. What makes me passionate about games is the combination of technology and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments. I always tell a story.</p>
+    
+        <div class="blocker"></div>
+      </div>
+    </section>
+
+
+      <section class="container-fluid articlesbg">
+        <div class="container">
+          <h3 class="text-centre">LATEST ARTICLES</h3>
+            
+          <div class="row">
+            <div></div>
+                
+
+                    <div class="col-md-4 text-center">
+                      <img class="img-fluid" src="images/port3.jpg" alt=“portrait”
+                      src="http://206.189.45.97/~mesh1/wp-content/themes/starter_for_josh_site/images/port1.jpg">
+                        <h4 class="article-title">A PORTRAIT</h4>
+                        <p class="date" >12 December 2020</p>
+                        <p> STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+                        </p>
+<a class= "readmore" href="#" >CONTINUE READING</a>
+                    </div>
+
+                   
+                <div class="col-md-4 text-center">
+                  <img class="img-fluid" src="images/port4.jpg" alt=“portrait”
+                  src="http://206.189.45.97/~mesh1/wp-content/themes/starter_for_josh_site/images/port2.jpg">
+                    <h4 class="article-title">A MODEL</h4>
+                    <p class="date" >12 December 2020</p>
+                    <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+                    </p>
+<a class= "readmore" href="#" >CONTINUE READING</a>
+                </div>
+
+                <div class="col-md-4 text-center">
+                  <img class="img-fluid" src="images/port5.jpg" alt=“portrait” 
+                  src="http://206.189.45.97/~mesh1/wp-content/themes/starter_for_josh_site/images/port3.jpg">
+                    <h4 class="article-title">A GAMES DESIGN</h4>
+                    <p class="date" >12 December 2020</p>
+                    <p class="article-p">STS has never been shy of seeking new terrains of investigation. More and more STS scholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+                    
+                    </p>
+                    <a class= "readmore" href="#" >CONTINUE READING</a>
+                 
+                
+            </div>
+           </section>
+           <?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
